5e6fea22c70c7d4c766f32c564ca2deef1ae1932
remdui <remyduijsens@gmail.com>
2024-10-21 13:59:52 +0200

    Add linter

--- src/preprocessing/dataset.py
+++ src/preprocessing/dataset.py
@@ -2,18 +2,49 @@ import pandas as pd
 from torch.utils.data import Dataset
 
 class FreeSurferDataset(Dataset):
+    """
+    This module contains the FreeSurferDataset class which is used to load the FreeSurfer dataset.
+    """
+
     def __init__(self, csv_file, covariates_count, transform=None):
+        """
+        Constructor for the FreeSurferDataset class.
+        """
         self.data = pd.read_csv(csv_file)
         self.covariates_count = covariates_count
         self.transform = transform
 
     def __len__(self):
+        """
+        Returns the length of the dataset.
+        """
         return len(self.data)
 
     def __getitem__(self, idx):
+        """
+        Returns the brain measures and covariates for a given index.
+        """
         brain_measures = self.data.iloc[idx, 1:-self.covariates_count].values  # Features
         covariates = self.data.iloc[idx, -self.covariates_count:].values  # Covariates
 
         if self.transform:
             brain_measures = self.transform(brain_measures)
         return brain_measures, covariates
+
+    def get_num_features(self):
+        """
+        Returns the number of features in the dataset.
+        """
+        return self.data.shape[1] - self.covariates_count
+
+    def get_num_covariates(self):
+        """
+        Returns the number of covariates in the dataset.
+        """
+        return self.covariates_count
+
+    def get_num_samples(self):
+        """
+        Returns the number of samples in the dataset.
+        """
+        return self.data.shape[0]
